69cf2610541c89e576b9b5c5e47cd3e706637499
jeenskki <madrine27@naver.com>
2020-07-20 17:21:48 +0900

    index changed

--- codeTogether/codeTogether/urls.py
+++ codeTogether/codeTogether/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sign/', include('sign.urls'))
+    path('sign/', include('sign.urls')),
+    path('main/', include('main.urls')),
 ]
--- main/urls.py
+++ main/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signup_mentee/', views.signup_mentee),
-    path('signup_mentor/', views.signup_mentor),
+    path('/', views.index,),
 ]
