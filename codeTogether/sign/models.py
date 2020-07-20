d622d8ee1e3fe1f49af7c272483310e69ce35001
jeenskki <madrine27@naver.com>
2020-07-20 14:32:28 +0900

    main.html 작업중

---
+++ codeTogether/sign/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+#회원정보 DB
+
+class User(models.Model):
+    username = models.CharField(max_length=64, verbose_name = '사용자명')
+    user_id = models.CharField(max_length=64, verbose_name = '사용자명')
+    user_pw = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    birth = models.IntegerField(length=6)
+    user_type = models.IntegerField(max_length=255)
+    
+ 
+    def __str__(self):
+        return self.name + str(self.age)
