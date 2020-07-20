ab93ffed19eda7294f417564fd4928a38b7a998a
mins0022 <suon94@gmail.com>
2020-07-20 17:33:32 +0900

    login 기능 ( session 작업 필요)

--- main/views.py
+++ main/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def main(request):
-    print(request.method)
     return render(request,'main/index.html')
--- sign/views.py
+++ sign/views.py
@@ -1,22 +1,65 @@
+import json
+from .models import User
+from django.views import View
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
-from django.http import HttpResponse
+
+#회원가입 기능
+class SignUpView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        try:
+            if User.objects.filter(user_id = data['user_id']).exists() :
+                return JsonResponse({"message" : "USER_ALREADY_EXIST"}, status = 401)
+            User(
+                user_id=data['user_id'],
+                email=data['email'],
+                password=data['password'],
+            ).save()
+            return HttpResponse(status = 200)
+        except KeyError:
+            return JsonResponse({'message' : "INVALID_KEYS"}, status = 400)
+#get 방식으로 요청시 db에 있는 데이터와 함게 
+    def get(self, request):
+        # users = User.objects.values()
+        # data =  JsonResponse({"data" : list(users)}, status = 200)
+        return render(request,'sign/signup.html')
+
+#로그인 기능
+class LoginView(View):
+    def post(self, request):
+        user_id = request.POST.get('user_id')
+        user_pw = request.POST.get('user_pw')
+        try:
+            if User.objects.filter(user_id = user_id).exists() :
+                if user_pw == User.objects.get(user_id=user_id).user_pw:
+                    return HttpResponse(status=200)
+                return HttpResponse(status=401)
+            return HttpResponse(status=401)
+        except KeyError:
+            return JsonResponse({'message' : "INVALID_KEYS"}, status = 400)
+    def get(self,request):
+        return render(request, 'sign/signin.html')
+            
+
+
+
 
 # Create your views here.
-def login(request):
-    return render(request, 'sign/signin.html')
+# def login(request):
+#     return render(request, 'sign/signin.html')
 
-def login_post(request):
-    user_id = request.GET.get('user_id')
-    user_pw = request.GET.get('user_pw')
-    print(user_id, user_pw)
-    request.session['user_id'] = user_id
-    return HttpResponse("로그인 완료")
+# def login_post(request):
+#     user_id = request.GET.get('user_id')
+#     user_pw = request.GET.get('user_pw')
+#     print(user_id, user_pw)
+#     request.session['user_id'] = user_id
+#     return HttpResponse("로그인 완료")
 
-def logout(request):
-    request.session['user_id'] = None
-    request.session.clear()
-    return HttpResponse("로그아웃")
+# def logout(request):
+#     request.session['user_id'] = None
+#     request.session.clear()
+#     return HttpResponse("로그아웃")
 
-def singup(request):
-    return render(request,'signup.html')
-
+# def singup(request):
+#     return render(request,'signup.html')
