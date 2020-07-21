4f3f9a460949bc6750a6bf018710beeb086eb1d5
mins0022 <suon94@gmail.com>
2020-07-21 12:21:24 +0900

    회원가입/로그인 세션 추가 중 /회원가입시 user_type cnrk vlfdy

--- sign/views.py
+++ sign/views.py
@@ -7,36 +7,51 @@ from django.views import View
 class SignUpView(View):
     def post(self, request):
         user_id = request.POST.get('user_id')
-        try:
-            if User.objects.filter(user_id = data['user_id']).exists() :
-                return JsonResponse({"message" : "USER_ALREADY_EXIST"}, status = 401)
-            User(
-                user_id=data['user_id'],
-                email=data['email'],
-                password=data['password'],
-            ).save()
-            return HttpResponse(status = 200)
-        except KeyError:
-            return JsonResponse({'message' : "INVALID_KEYS"}, status = 400)
+        user_pw = request.POST.get('user_pw')
+        re_user_pw = request.POST.get('re_user_pw')
+        username = request.POST.get('username')
+        user_email = request.POST.get('email')
+        birth = request.POST.get('birth')
+        user_type = request.POST.get('user_type') #user_type을 signup_ 페이지에서 설정후에 가져와야하는데 어떻게 넣어주지
+
+        if user_pw == re_user_pw:
+            try:
+                if User.objects.filter(user_id = user_id).exists() :
+                    return JsonResponse({"message" : "USER_ALREADY_EXIST"}, status = 401)
+                User(
+                    user_id=user_id,
+                    email=email,
+                    password=password,
+                    username =username,
+                    email=user_email,
+                    birth=birth,
+                    user_type=user_type
+                ).save()
+                return HttpResponse(status = 200)
+            except KeyError:
+                return JsonResponse({'message' : "INVALID_KEYS"}, status = 400)
+        else: HttpResponse("비밀번호 틀림")
 #get 방식으로 요청시 db에 있는 데이터와 함게 
-    def get(self, request):
-        return render(request,'sign/signup.html')
+    def get_tea(self, request):
+        return render(request,'sign/signup_tea.html')
+    
+    def get_stu(self, request):
+        return render(request,'sign/signup_sut.html')
 
 #로그인 기능
 class LoginView(View):
     def post(self, request):
         user_id = request.POST.get('user_id')
         user_pw = request.POST.get('user_pw')
-        # print(User.objects.get(user_id=user_id))
         try:
             if User.objects.filter(user_id = user_id).exists() :
-                
                 if user_pw == User.objects.get(user_id=user_id).user_pw:
+                    user_type = User.objects.get(user_id).user_type
                     return HttpResponse('0')
                 return HttpResponse('1')
             return HttpResponse('2')
         except KeyError:
-            return JsonResponse({'message' : "INVALID_KEYS"}, status = 400)\
+            return JsonResponse({'message' : "INVALID_KEYS"}, status = 400)
        
         # return HttpResponse(user_id)
     def get(self,request):
